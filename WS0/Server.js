a5b28ce8e4be35581139767c11c150970f41c79a
Jari Kovalainen <itse@jarikovalainen.com>
2022-01-27 08:20:12 +0200

    Update Server.js

--- WS0/Server.js
+++ WS0/Server.js
@@ -7,4 +7,4 @@ http
     response.write("Hello World!\n"); //write a response to the client
     response.end("This is the end"); //end the response
   })
-  .listen(8081); //the server object listens on port 8080
+  .listen(8081); //the server object listens on port 8081
