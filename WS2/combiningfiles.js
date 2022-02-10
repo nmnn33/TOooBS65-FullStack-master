a0abbc775bc07ce40d9eb65a8accfa607659966a
nmnn33 <tommy123tran@gmail.com>
2022-02-10 09:38:34 +0200

    dasd

--- WS2/combiningfiles.js
+++ WS2/combiningfiles.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var data = fs.writeFileSync("combinacion.txt", "");
 var data2 = fs.readFileSync("teksti.txt"); //Lisätään teksti.txt combinacion.txt
 var data3 = fs.readFileSync("teskti2.txt"); //Lisätään teksti2.txt combinacion.txt
-fs.writeFileSync("combinacion.txt", data2 + data3);
+fs.writeFileSync("combinacion.txt", data2 + data3); //combinacion
 fs.appendFileSync('combinacion.txt', "\nI wrote this!");
 var luku = fs.readFileSync('combinacion.txt');
 console.log(luku.toString());
