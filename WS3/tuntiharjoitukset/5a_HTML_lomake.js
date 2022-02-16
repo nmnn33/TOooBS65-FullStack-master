e83fd093de3af507ecfe53b4b57a57123ed608e6
Jari Kovalainen <itse@jarikovalainen.com>
2022-02-16 14:44:02 +0200

    HTMLForn päivitetty

--- WS3/tuntiharjoitukset/5a_HTML_lomake.js
+++ WS3/tuntiharjoitukset/5a_HTML_lomake.js
@@ -5,6 +5,7 @@ var app = express();
 // Require the module required for using form data
 var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // create application/x-www-form-urlencoded parser
 app.use(
@@ -17,6 +18,28 @@ app.use(
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/public/HTMLform.html");
 });
+
+// Uusi POST-tyyppiseen sivupyyntöön reagoiva reitti
+app.post("/kirjaudu", function (req, res) {
+    console.log(req.body);
+    var email = req.body.email;
+    var pass = req.body.pass;
+    // Jos tunnukset ovat oikeat, ohjataan käyttäjä uuteen reittiin
+    if (email === "onni@sci.fi" && pass === "opiskelija") {
+        res.redirect("/userpage");
+    }
+    res.send("Lähetit lomakkeen! Email: " + email + " Salis: " + pass);
+});
+
+// Uusi reitti sisäänkirjautuneelle käyttäjälle.
+app.get("/userpage", function (req, res) {
+    res.send("You are now logged in!");
+});
+
+app.get("*", function (req, res) {
+    res.send("Cant find the requested page", 404);
+});
+
 
 
 app.listen(8081, function () {
