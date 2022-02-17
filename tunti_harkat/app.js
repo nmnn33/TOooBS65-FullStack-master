32895aad8f0e0454759bd20b101e46afc9b1229b
nmnn33 <tommy123tran@gmail.com>
2022-02-17 09:03:36 +0200

    iuy

---
+++ tunti_harkat/app.js
@@ -0,0 +1,4 @@
+//load module logger.js
+const log = require('./logger.js');
+
+log('katso logger.js');
---
+++ tunti_harkat/logger.js
@@ -0,0 +1,8 @@
+var url = 'https://mylogger.io/log';
+
+function log(message) {
+    console.log(message);
+}
+
+//exports log function
+module.exports = log;
